27edd9a89d1e7a7a1c0e9a2119c114a42850fb74
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-01 19:08:00 -0300

    feat: add pages home, quiz and results

---
+++ src/pages/Quiz.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+const Quiz: React.FC = () => {
+
+    return (
+        <div>
+        <Header showLogo={true} />
+      </div>
+    );
+}
+
+export default Quiz;
---
+++ src/pages/Results.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+const Results: React.FC = () => {
+
+    return (
+        <div>
+        <Header/>
+      </div>
+    );
+}
+
+export default Results;
