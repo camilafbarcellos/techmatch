5fa3c57ce25e1d87e117b0c243283834a3468daf
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-02 15:32:50 -0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import './App.css';
-import Home from './pages/Home';
+import Router from './router/Router';
 
 const App: React.FC = () => {
   return (
-    <Home/>
+    <Router/>
   );
 };
 
