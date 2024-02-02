dd979be4e589a58c982f48f00858e06e1da30b34
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-02 18:05:45 -0300

    feat: add home page components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import { Box, Container } from '@mui/material';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+import LandingContainer from '../components/LandingContainer';
 
 const Home: React.FC = () => {
+
   return (
     <Container maxWidth='md' sx={{ px: { xs: 2, md: 4 } }}>
       <Box sx={{ my: 4 }}>
         <Header />
+        <LandingContainer/>
+        <Footer />
       </Box>
     </Container>
   );
