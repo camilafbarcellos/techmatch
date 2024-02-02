cff4befdb1c02588c16d68759ad5807248f2a875
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-02 17:14:33 -0300

    feat: ad mui theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react';
 import './App.css';
 import Router from './router/Router';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1F2024',
+      light: '#71727A'
+    },
+    secondary: {
+      main: '#430F7E',
+      dark: '#683e97',
+      light: '#d9cfe5'
+    },
+    info: {
+      main: '#FE7E15',
+      light: '#fee5d0'
+    }
+  },
+  typography: {
+    fontFamily: ['Poppins', 'Roboto', 'sans-serif', 'Arial'].join(',')
+  }
+});
 
 const App: React.FC = () => {
   return (
-    <Router/>
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
   );
 };
 
