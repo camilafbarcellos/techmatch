9bdf5d2e8d1b8c87f335fc6152952685872f53d8
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-05 15:33:12 -0300

    refactor: refactor navigation button

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Box, Button as MuiButton } from '@mui/material';
 
-interface ButtonProps {
+interface NavigationButtonProps {
   text: string;
   onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+const NavigationButton: React.FC<NavigationButtonProps> = ({ text, onClick }) => {
   return (
     <Box sx={{ textAlign: 'center', mt: 4, pb: 2 }}>
       <MuiButton className='action-button' variant='contained' color='secondary' onClick={onClick}>
@@ -16,4 +16,4 @@ const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   );
 };
 
-export default Button;
+export default NavigationButton;
