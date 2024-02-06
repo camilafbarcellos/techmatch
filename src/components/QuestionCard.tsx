2636c1b901bf30ce32330a98f01841d7d0fb2533
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-06 18:07:02 -0300

    style: change question quote color

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -10,8 +10,8 @@ const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
     return (
         <Box sx={{ textAlign: 'center', width: '75%', mt: 4 }}>
             <Card variant='outlined'>
-                <CardContent sx={{ display: 'flex', alignItems: 'center', justifyContent: 'left', gap: '1rem' }}>
-                    <QuoteIcon color='primary' sx={{justifyContent: 'flex-start'}} />
+                <CardContent sx={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', gap: '1rem' }}>
+                    <QuoteIcon color='disabled' sx={{justifyContent: 'flex-start'}} />
                     <Typography variant='h6' color='primary'>{question}</Typography>
                 </CardContent>
             </Card>
