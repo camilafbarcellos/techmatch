c03c7c62f53d63f1618555845241af504f1074ed
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-15 20:48:07 -0300

    style: change footer margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Box, Link } from '@mui/material';
 
 const Footer: React.FC = () => {
   return (
-    <Box sx={{ textAlign: 'center', mt: 4, pb: 2 }}>
+    <Box sx={{ textAlign: 'center', pb: 1 }}>
       <Link href='https://br.linkedin.com/in/camilafbarcellos' variant='body2' color='primary.light' underline='none' target='_blank'>
         &copy; {new Date().getFullYear()} Camila Barcellos
       </Link>
