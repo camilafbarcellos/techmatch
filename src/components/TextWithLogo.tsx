291bf0964c7e65f17e04951f5bde0f04f719c0dc
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-16 18:58:09 -0300

    style: change landing page text variants

--- src/components/TextWithLogo.tsx
+++ src/components/TextWithLogo.tsx
@@ -18,12 +18,12 @@ const TextWithLogo: React.FC<TextWithLogoProps> = ({ title, text, handleButton }
             flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
         }}>
             <Logo className='large-logo' />
-            <Typography variant='h4' fontFamily='Comfortaa' fontWeight='700' component='div' >
+            <Typography variant='h3' fontFamily='Comfortaa' fontWeight='700' component='div' >
                 <span style={{ color: '#430F7E' }}>&gt;</span>
                 <span style={{ color: '#FE7E15' }}>_</span>
                 &nbsp; {`${title}`}
             </Typography>
-            <Typography variant='body1' color='primary.light'>
+            <Typography variant='subtitle1' color='primary.light'>
                 {`${text}`}
             </Typography>
             <NavigationButton text={'Começar'} onClick={handleButton} />
