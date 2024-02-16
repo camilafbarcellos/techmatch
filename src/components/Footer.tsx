ba6a0e4840d90f97fcb1e9202f11b843dc6766a6
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-16 18:56:18 -0300

    style: change footer text variant

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Box, Link } from '@mui/material';
 const Footer: React.FC = () => {
   return (
     <Box sx={{ textAlign: 'center', pb: 1 }}>
-      <Link href='https://br.linkedin.com/in/camilafbarcellos' variant='body2' color='primary.light' underline='none' target='_blank'>
+      <Link href='https://br.linkedin.com/in/camilafbarcellos' variant='caption' color='primary.light' underline='none' target='_blank'>
         &copy; {new Date().getFullYear()} Camila Barcellos
       </Link>
     </Box>
