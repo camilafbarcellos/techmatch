ef4727df274b7786eab6d69d3c723fb51e025b4c
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-16 19:02:46 -0300

    style: remove question card gap

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -10,7 +10,7 @@ const QuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
     return (
         <Box sx={{ textAlign: 'center', width: '75%' }}>
             <Card variant='outlined'>
-                <CardContent sx={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', gap: '1rem' }}>
+                <CardContent sx={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
                     <QuoteIcon color='disabled' sx={{justifyContent: 'flex-start'}} />
                     <Typography variant='h6' color='primary'>{question}</Typography>
                 </CardContent>
