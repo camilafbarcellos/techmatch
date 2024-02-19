226a7fd13b10dd7787551497527683ed163d8cba
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-19 14:49:19 -0300

    feat: make header link to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, Box, Typography } from '@mui/material';
 import Logo from './Logo';
 import '../assets/styles/logo.css';
+import { Link } from 'react-router-dom';
 
 interface HeaderProps {
   showLogo?: boolean;
@@ -12,15 +13,15 @@ const Header: React.FC<HeaderProps> = ({ showLogo = false }) => {
     <AppBar position='static' sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
       <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         {showLogo && (
-          <Box component='span' sx={{ alignItems: 'center' }}>
+          <Box component={Link} to='/' sx={{ alignItems: 'center' }}>
             <Logo className='header-logo' />
           </Box>
         )}
         <Typography variant='h4' color='secondary' fontFamily='Comfortaa' fontWeight='700'>
-          tech
+          <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>tech</Link>
         </Typography>
         <Typography variant='h4' color='info.main' fontFamily='Comfortaa' fontWeight='700'>
-          match
+          <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>match</Link>
         </Typography>
       </Toolbar>
     </AppBar>
