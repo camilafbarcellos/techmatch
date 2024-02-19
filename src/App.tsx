ae32551d407b1292a36429c9889f88cfa26f8279
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-19 15:13:06 -0300

    build: add Vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import Router from './router/Router';
 import { ThemeProvider } from '@mui/material';
 import theme from './utils/MuiTheme';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router />
+      <SpeedInsights />
     </ThemeProvider>
   );
 };
