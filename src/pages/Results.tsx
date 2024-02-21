778208f0f766c574330a6181a0b1740d0eb86561
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-20 23:41:34 -0300

    fix: fix merge conflicts

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -10,14 +10,9 @@ import {
   calculateCategoryPercentage,
 } from '../utils/matchingAlgorithm';
 import TextWithLogo from '../components/TextWithLogo';
-<<<<<<< HEAD
 import MainCircleProgress from '../components/MainCircleProgress';
 import CategoryBox from '../components/CategoryBox';
 import NavigationButton from '../components/NavigationButton';
-=======
-import SecondaryCircleProgress from '../components/SecondaryCircleProgress';
-import MainCircleProgress from '../components/MainCircleProgress';
->>>>>>> 03cb738bef5269eb9a6118d4ece35f41b2a9e585
 
 const Results: React.FC = () => {
 
@@ -51,16 +46,9 @@ const Results: React.FC = () => {
 
   return (
     <Container
-<<<<<<< HEAD
       sx={{
         display: 'flex', flexDirection: 'column', gap: '2rem',
         minHeight: '100vh', px: { xs: 2, md: 4 }, maxWidth: 'md'
-=======
-      maxWidth='md'
-      sx={{
-        display: 'flex', flexDirection: 'column',
-        minHeight: '100vh', px: { xs: 2, md: 4 },
->>>>>>> 03cb738bef5269eb9a6118d4ece35f41b2a9e585
       }}
     >
       <Header />
@@ -71,7 +59,6 @@ const Results: React.FC = () => {
           justifyContent: 'center', alignItems: 'center'
         }}
       >
-<<<<<<< HEAD
         <TextWithLogo title={title} text={text} />
       </Box>
 
@@ -79,20 +66,10 @@ const Results: React.FC = () => {
         display: 'flex', alignItems: 'center', gap: '1rem', my: 'auto',
         justifyContent: 'center', flexDirection: 'column',
       }}>
-=======
-        <TextWithLogo title={title} text={text} buttonAction='Responder à pesquisa' handleButton={handleButton} />
-      </Box>
-
-      <Box sx={{
-        display: 'flex', alignItems: 'center', gap: '1rem',
-        justifyContent: 'center', flexDirection: 'column',
-      }} my={4}>
->>>>>>> 03cb738bef5269eb9a6118d4ece35f41b2a9e585
         <MainCircleProgress value={totalScore} text={`${totalScore}%`} />
         <Typography variant='h4' fontWeight='600'>
           TI
         </Typography>
-<<<<<<< HEAD
       </Box>
 
       <Box sx={{
@@ -106,37 +83,6 @@ const Results: React.FC = () => {
       </Box>
 
       <NavigationButton text='Responder à pesquisa' onClick={handleButton} />
-=======
-
-        <Box display='flex' justifyContent='space-around'>
-          <Box textAlign='center'>
-            <Typography variant='h6' fontWeight='600'>
-              Desenvolvimento
-            </Typography>
-            <SecondaryCircleProgress value={categoryPercentage('Desenvolvimento')} text={`${categoryPercentage('Desenvolvimento')}%`} />
-          </Box>
-          <Box textAlign='center'>
-            <Typography variant='h6' fontWeight='600'>
-              Infraestrutura de TI
-            </Typography>
-            <SecondaryCircleProgress value={categoryPercentage('Infraestrutura de TI')} text={`${categoryPercentage('Infraestrutura de TI')}%`} />
-          </Box>
-          <Box textAlign='center'>
-            <Typography variant='h6' fontWeight='600'>
-              Ciência de Dados
-            </Typography>
-            <SecondaryCircleProgress value={categoryPercentage('Ciência de Dados')} text={`${categoryPercentage('Ciência de Dados')}%`} />
-          </Box>
-          <Box textAlign='center'>
-            <Typography variant='h6' fontWeight='600'>
-              Cibersegurança
-            </Typography>
-            <SecondaryCircleProgress value={categoryPercentage('Cibersegurança')} text={`${categoryPercentage('Cibersegurança')}%`} />
-          </Box>
-        </Box>
-      </Box>
-
->>>>>>> 03cb738bef5269eb9a6118d4ece35f41b2a9e585
       <Footer />
     </Container>
   );
