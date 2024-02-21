7fa76e1487f7ca74698e99135724e96a18c01599
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-20 23:08:48 -0300

    feat: add secondary circular progress bar

---
+++ src/components/SecondaryCircleProgress.tsx
@@ -0,0 +1,35 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+interface SecondaryCircleProgressProps {
+    value: number;
+    text: string;
+}
+
+const SecondaryCircleProgress: React.FC<SecondaryCircleProgressProps> = ({ value, text }) => {
+    return (
+        <Box width='6rem' height='6rem'>
+            <CircularProgressbar
+                value={value}
+                text={text}
+                styles={{
+                    path: {
+                        stroke: '#FE7E15'
+                    },
+                    trail: {
+                        stroke: '#FEBE8A'
+                    },
+                    text: {
+                        fill: '#1F2024',
+                        fontFamily: ['Poppins', 'Roboto', 'sans-serif', 'Arial'].join(','),
+                        fontWeight: '600'
+                    },
+                }}
+            />
+        </Box>
+    );
+};
+
+export default React.memo(SecondaryCircleProgress);
