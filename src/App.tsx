eed703e1fc577464d17bb74ccc89c5eb41c36583
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-26 23:00:03 -0300

    feat: add data fetching to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import * as React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Router from './router/Router';
 import { ThemeProvider } from '@mui/material';
 import theme from './utils/MuiTheme';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Question } from './types/question';
+import { fetchData } from './utils/fetchData';
+import { AppContext } from './context/AppContext';
 
 const App: React.FC = () => {
+
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    fetchData().then((data: Question[]) => {
+      setQuestions(data);
+    });
+  }, []);
+
+  // Memoized value to store the fetched questions
+  const value = useMemo(() => ({ questions }), [questions]);
+
   return (
     <ThemeProvider theme={theme}>
-      <Router />
+      <AppContext.Provider value={value}>
+        <Router />
+      </AppContext.Provider>
       <SpeedInsights />
     </ThemeProvider>
   );
