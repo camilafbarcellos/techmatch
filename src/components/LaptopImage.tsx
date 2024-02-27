736d258d4b22b766f87809f752fc2d0aad69d82f
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-26 23:46:27 -0300

    feat: add laptop image component

---
+++ src/components/LaptopImage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../assets/styles/images.css';
+
+interface LaptopImageProps {
+    className: string;
+}
+
+const LaptopImage: React.FC<LaptopImageProps> = ({ className = '' }) => {
+    return (
+        <img
+            className={`${className}`}
+            alt='Laptop with code and coffee'
+            src={require('../assets/images/laptop.png')}
+        />
+    );
+};
+
+export default React.memo(LaptopImage);
