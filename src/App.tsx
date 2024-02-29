c337f2035d8d2d2a977675d176cfac04172997cf
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-29 19:13:00 -0300

    refactor: refactor api request

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Router from './router/Router';
 import { ThemeProvider } from '@mui/material';
 import theme from './utils/MuiTheme';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Question } from './types/question';
-import { fetchData } from './utils/fetchData';
 import { AppContext } from './context/AppContext';
+import { api } from './utils/api';
 
 const App: React.FC = () => {
 
   const [questions, setQuestions] = useState<Question[]>([]);
 
-  useEffect(() => {
-    fetchData().then((data: Question[]) => {
-      setQuestions(data);
-    });
+  const fetchData = useCallback(async () => {
+    try {
+      await api.get('/questions')
+        .then(response => {
+          setQuestions(response.data);
+        });
+    } catch (error) {
+      console.error('Error fetching questions:', error);
+    }
   }, []);
 
-  // Memoized value to store the fetched questions
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   const value = useMemo(() => ({ questions }), [questions]);
 
   return (
---
+++ src/utils/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL as string,
+    timeout: 15000,
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+});
