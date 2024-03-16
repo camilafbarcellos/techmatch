375d35120c0e3f3520459cab2e6b4d5175133f25
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-16 12:12:41 -0300

    fix: increase api timeout to prevent error when idle

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL as string,
-    timeout: 60000,
+    timeout: 70000,
     headers: {
         'Content-Type': 'application/json; charset=utf-8'
     }
