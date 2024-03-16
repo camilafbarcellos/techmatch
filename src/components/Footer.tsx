e3c0e22536e37d730c7355fc2c171c5e742ba582
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-03-16 11:05:37 -0300

    style: adjust footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Box, Link } from '@mui/material';
 
 const Footer: React.FC = () => {
   return (
-    <Box sx={{ textAlign: 'center', mb: -0.5 }}>
+    <Box component='footer' sx={{ textAlign: 'center', mb: -0.5, px: '24px' }}>
       <Link href='https://br.linkedin.com/in/camilafbarcellos' variant='caption' color='primary.light' underline='none' target='_blank'>
         &copy; {new Date().getFullYear()} Camila Barcellos
       </Link>
